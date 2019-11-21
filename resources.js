006b70f18a3ae8b207b377a20e3c206d50c0ac61
NikhilPuri299 <58026514+NikhilPuri299@users.noreply.github.com>
2019-11-21 13:43:21 +0530

    Create resources.js

---
+++ resources.js
@@ -0,0 +1,37 @@
+import "@ui5/webcomponents/dist/ShellBar.js"; // loads ui5-shellbary
+import "@ui5/webcomponents-base/dist/features/browsersupport/IE11.js"; // This will enable Edge and IE11 support for all Web Components below
+import "@ui5/webcomponents/dist/Badge.js";
+import "@ui5/webcomponents/dist/BusyIndicator.js";
+import "@ui5/webcomponents/dist/Button.js";
+import "@ui5/webcomponents/dist/Card.js";
+import "@ui5/webcomponents/dist/CheckBox.js";
+import "@ui5/webcomponents/dist/DatePicker.js";
+import "@ui5/webcomponents/dist/Dialog.js";
+import "@ui5/webcomponents/dist/Icon.js";
+import "@ui5/webcomponents/dist/Input.js";
+import "@ui5/webcomponents/dist/Label.js";
+import "@ui5/webcomponents/dist/Link.js";
+import "@ui5/webcomponents/dist/List.js";
+import "@ui5/webcomponents/dist/StandardListItem.js";
+import "@ui5/webcomponents/dist/CustomListItem.js";
+import "@ui5/webcomponents/dist/GroupHeaderListItem.js";
+import "@ui5/webcomponents/dist/MessageStrip.js";
+import "@ui5/webcomponents/dist/MultiComboBox.js";
+import "@ui5/webcomponents/dist/Panel.js";
+import "@ui5/webcomponents/dist/Popover.js";
+import "@ui5/webcomponents/dist/RadioButton.js";
+import "@ui5/webcomponents/dist/Select.js";
+import "@ui5/webcomponents/dist/Switch.js";
+import "@ui5/webcomponents/dist/TabContainer.js";
+import "@ui5/webcomponents/dist/Tab.js";
+import "@ui5/webcomponents/dist/TabSeparator.js";
+import "@ui5/webcomponents/dist/Table.js";
+import "@ui5/webcomponents/dist/TableColumn.js";
+import "@ui5/webcomponents/dist/TableRow.js";
+import "@ui5/webcomponents/dist/TableCell.js";
+import "@ui5/webcomponents/dist/TextArea.js";
+import "@ui5/webcomponents/dist/Timeline.js";
+import "@ui5/webcomponents/dist/Title.js";
+import "@ui5/webcomponents/dist/ToggleButton.js";
+
+import "@ui5/webcomponents/dist/json-imports/Themes.js";
